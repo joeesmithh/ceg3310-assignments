907ba79054dd7385e93bb9a8a89976142c0f7828
joeesmithh <josmth3030@gmail.com>
2025-09-22 20:23:12 -0400

    feat(Lab03): complete GETOP subroutine

--- Lab03/lab03.asm
+++ Lab03/lab03.asm
@@ -1,20 +1,56 @@
 .ORIG x3000
 ; = = = = = = = = = = = = = = = = =
 
-; Display prompt
+; Get first number
 LEA	R0,	PROMPT1
 PUTS
 JSR	GETNUM
 
-; Store number in R1
-AND R4, R4, #0
-ADD R4, R4, R0
+; Store number in R3
+AND R3, R3, #0
+ADD R3, R3, R0
+
+; Output new line
+LD R0, NEWLINE_CHAR
+OUT
+
+; Get operator
+LEA	R0,	PROMPT2
+PUTS
+JSR	GETOP
+
+; Store operator in R2
+AND R2, R2, #0
+ADD R2, R2, R0
+
+; Output new line
+LD R0, NEWLINE_CHAR
+OUT
+
+; Get second number
+LEA	R0,	PROMPT3
+PUTS
+JSR	GETNUM
+
+; Store second number in R1
+AND R1, R1, #0
+ADD R1, R1, R0
+
+; Store first number in R0
+AND R0, R0, #0
+ADD R0, R0, R3
+
+; Output new line
+LD R0, NEWLINE_CHAR
+OUT
 
 HALT
 ; - - - - - - - - - - - - - - - - -
-
+; MAIN VARS
+NEWLINE_CHAR .FILL x000A
 PROMPT1 .STRINGZ "Enter first number (0-99): "
-PROMPT2 .STRINGZ "Enter second number (0-99): "
+PROMPT2 .STRINGZ "Enter an operation (+, -, *): "
+PROMPT3 .STRINGZ "Enter second number (0-99): "
 ; = = = = = = = = = = = = = = = = =
 
 
@@ -45,16 +81,16 @@ ST	R1,	GETNUM_R1
 ST	R2,	GETNUM_R2
 ST	R7,	GETNUM_R7
 
-BRnzp INPUT ; Branch unconditionally on first call to INPUT
+BRnzp GETNUM_INPUT ; Branch unconditionally on first call to INPUT
 
-ERROR ; Branch target for invalid integer input
+GETNUM_ERROR ; Branch target for invalid integer input
 AND R0, R0, #0
 ADD R0, R0, x000A
 OUT ; Output new line
 LEA R0, ERROR_MSG
 PUTS ; Output error message
 
-INPUT
+GETNUM_INPUT
 
 GETC ; Input 1st character
 OUT  ; Output 1st character
@@ -62,9 +98,9 @@ LD R1, NEG_48
 ADD R2, R0, R1 ; R2 = 1st int
 
 ADD R1, R2, #-9 ; Validate 1st int <= 9
-BRp ERROR
+BRp GETNUM_ERROR
 ADD R1, R1, #9  ; Validate 1st int >= 0
-BRn ERROR
+BRn GETNUM_ERROR
 
 GETC ; Input 2nd character
 
@@ -81,9 +117,9 @@ LD R1, NEG_48
 ADD R0, R0, R1 ; R0 = 2nd int
 
 ADD R1, R0, #-9 ; Validate 2nd int <= 9
-BRp ERROR
+BRp GETNUM_ERROR
 ADD R1, R1, #9  ; Validate 2nd int >= 0
-BRn ERROR
+BRn GETNUM_ERROR
 
 ; (1st int x 10) + 2nd int
 AND R1, R1, #0
@@ -109,29 +145,80 @@ LD  R2, GETNUM_R2
 LD	R7,	GETNUM_R7
 
 RET
-
-; GETNUM Variables
+; - - - - - - - - - - - - - - - - -
+; GETNUM VARS
 GETNUM_R1 .BLKW	#1
 GETNUM_R2 .BLKW	#1
 GETNUM_R7 .BLKW	#1
 NEG_48 .FILL xFFD0
 ERROR_MSG .STRINGZ "Invalid input! Stay within (0-99): "
-
 ; END GETNUM = = = = = = = = = = = = = = = = =
 
 
 
 
-; = = = = = = = = = = = = = = = = =
+; GETOP = = = = = = = = = = = = = = = = = = =
+; Stores user input operator (+, -, or *) in R0.
+; Input is validated from within the subroutine
+; until the input is correct.
 GETOP
 
-; GETOP CODE HERE
+; Callee saves
+ST R1, GETOP_R1
+ST R7, GETOP_R7
+
+BRnzp GETOP_INPUT ; Branch unconditionally past error message
+
+GETOP_ERROR ; Branch target for invalid input
+AND R0, R0, #0
+ADD R0, R0, x000A
+OUT ; Output new line
+LEA R0, OP_ERROR_MSG
+PUTS
+
+GETOP_INPUT
+
+; User input
+GETC
+OUT
+
+; Validate input
+LD R1, TIMES_CHAR
+NOT R1, R1
+ADD R1, R1, #1
+ADD R1, R0, R1
+BRz GETOP_SUCCESS
+
+LD R1, PLUS_CHAR
+NOT R1, R1
+ADD R1, R1, #1
+ADD R1, R0, R1
+BRz GETOP_SUCCESS
+
+LD R1, MINUS_CHAR
+NOT R1, R1
+ADD R1, R1, #1
+ADD R1, R0, R1
+BRz GETOP_SUCCESS
+
+BRnzp GETOP_ERROR ; Branch to error message, try new input
+
+GETOP_SUCCESS ; Branch target for input validation
+
+; Callee loads
+LD	R1,	GETOP_R1
+LD	R7,	GETOP_R7
 
 RET
 ; - - - - - - - - - - - - - - - - -
-
 ; GETOP VARS
-; = = = = = = = = = = = = = = = = =
+GETOP_R1 .BLKW	#1
+GETOP_R7 .BLKW	#1
+TIMES_CHAR .FILL x002A
+PLUS_CHAR .FILL x002B
+MINUS_CHAR .FILL x002D
+OP_ERROR_MSG .STRINGZ "Invalid input! Enter one of (+, -, or *): "
+; END GETOP = = = = = = = = = = = = = = = = =
 
 
 
