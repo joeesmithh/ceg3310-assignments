6417f8b6b27b1d3fa6efb0076474f76c0e478ca5
joeesmithh <josmth3030@gmail.com>
2025-10-07 22:07:19 -0400

    complete practice midterm 3 coding problems

--- practice-midterm-3/q1.asm
+++ practice-midterm-3/q1.asm
@@ -1,7 +1,44 @@
+; Write an LC3 assembly program originating at memory address x3000 that will calculate the volume of a rectangular prism. The volume of a rectangular prism is:
+; volume = length x width x height
+; The length, width, and height must be stored inside of a .FILL inside of your program. After calculating the volume, place the result into memory address x8000.
+; Example, if you have the following .FILL locations in your program:
+; LENGTH 	.FILL #5
+; WIDTH	.FILL #2
+; HEIGHT	.FILL #27
+
 .ORIG x3000
+
+LD R0, LENGTH   ; R0 = length
+LD R1, WIDTH    ; R1 = width
+
+AND R2, R2, #0  ; R2 = 0
+
+LW
+ADD R2, R2, R0
+ADD R1, R1, #-1
+BRnp LW
+
+; R2 = length x width
+
+LD R1, HEIGHT   ; R1 = height
+ADD R0, R2, #0  ; R0 = length x width
+AND R2, R2, #0  ; R2 = 0
+
+LWH
+ADD R2, R2, R0
+ADD R1, R1, #-1
+BRnp LWH
+
+; R2 = length x width x height
+
+STI	R2, RESULT
 
 HALT
 
 ; Variables
+LENGTH .FILL #5
+WIDTH .FILL #2
+HEIGHT .FILL #27
+RESULT .FILL x8000
 
 .END
--- practice-midterm-3/q2.asm
+++ practice-midterm-3/q2.asm
@@ -1,7 +1,90 @@
 .ORIG x3000
 
+START
+
+; Input int 1
+LEA R0, PROMPT
+PUTS
+GETC
+OUT
+ADD R1, R0, #0
+
+; New line
+LD, R0, NL
+OUT
+
+; Input int 2
+LEA R0, PROMPT
+PUTS
+GETC
+OUT
+ADD R2, R0, #0
+
+; New line
+LD, R0, NL
+OUT
+
+; Output "R1 / R2 = "
+ADD R0, R1, #0
+OUT
+
+LEA R0, DIV
+PUTS
+
+ADD R0, R2, #0
+OUT
+
+LEA R0, EQ
+PUTS
+; ------------------------------
+
+; Convert char ints to numbers
+LD R0, NEG48
+ADD R1, R1, R0
+ADD R2, R2, R0
+
+; R3 = 0
+AND R3, R3, #0
+
+; R2 = -R2
+NOT R2, R2
+ADD R2, R2, #1
+
+; Subtract until nothing left to subtract
+SUB
+ADD R1, R1, R2
+BRzp INC
+
+BRnzp END
+
+INC
+ADD R3, R3, #1
+BRnzp SUB
+
+END
+; ------------------------------
+
+; Convert result to char and print
+LD R0, POS48
+ADD R3, R3, R0
+ADD R0, R3, #0
+OUT
+
+; New line
+LD, R0, NL
+OUT
+
+; Back to start
+BRnzp START
+
 HALT
 
 ; Variables
+NEG48 .FILL xFFD0
+POS48 .FILL x0030
+NL .FILL x000A
+PROMPT .STRINGZ	"Please enter a digit: "
+DIV .STRINGZ	" / "
+EQ .STRINGZ	" = "
 
 .END
