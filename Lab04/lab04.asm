1479536871afecc7c3219c04ed162120f96b19ea
joeesmithh <josmth3030@gmail.com>
2025-10-19 20:56:02 -0400

    lab04: remove TOTAL from global variables

--- Lab04/lab04.asm
+++ Lab04/lab04.asm
@@ -12,15 +12,15 @@ LEA	R4,	GLOBAL_VARS  ; R4 = global vars beg. address
 JSR MAIN
 
 ; Store result in TOTAL for debug purposes
-STR R0, R4, #2
+ST R0, TOTAL
 
 HALT
 
 ; Global Variables ----------------------------------
 STACK_PTR .FILL x6000
+TOTAL       .BLKW #1 ; final total      
 GLOBAL_VARS .BLKW #1 ; global vars start
 ARRAY_SIZE  .FILL #5 ; arraySize
-TOTAL       .BLKW #1 ; final total      
 ; END INITIALIZER CODE ===================================
 
 
@@ -33,7 +33,7 @@ MAIN
 ; Set MAIN return address at stack start
 STR	R7,	R6,	#0
 
-; Set current frame pointer (no previous frame to push)
+; Set current frame pointer to stack start (no previous frame to push)
 ADD R5, R6, #0
 
 ; Declare array
