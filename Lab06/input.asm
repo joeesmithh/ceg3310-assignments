5c2bac1a8715e4c1762d7a922fcf7a16715c1331
joeesmithh <josmth3030@gmail.com>
2025-11-15 14:33:45 -0500

    lab06: fix labels to match name of trap service routines

--- Lab06/input.asm
+++ Lab06/input.asm
@@ -1,30 +1,32 @@
 .ORIG x4000
 
+TRAP_INPUT
+
 ; Callee saves
-ST	R1,	SAVE_R1
-ST	R2,	SAVE_R2
-ST	R7,	SAVE_R7
+ST	R1,	INPUT_SAVE_R1
+ST	R2,	INPUT_SAVE_R2
+ST	R7,	INPUT_SAVE_R7
 
 BRnzp INPUT ; Branch unconditionally on first call to INPUT
 
-ERROR ; Branch target for invalid integer input
+INPUT_ERROR ; Branch target for invalid integer input
 AND R0, R0, #0
 ADD R0, R0, x000A
 OUT ; Output new line
-LEA R0, ERROR_MSG
-PUTS ; Output error message
+LEA R0, INPUT_INPUT_ERROR_MSG
+PUTS ; Output INPUT_ERROR message
 
 INPUT
 
 GETC ; Input 1st character
 OUT  ; Output 1st character
-LD R1, NEG_48
+LD R1, INPUT_NEG_48
 ADD R2, R0, R1 ; R2 = 1st int
 
 ADD R1, R2, #-9 ; Validate 1st int <= 9
-BRp ERROR
+BRp INPUT_ERROR
 ADD R1, R1, #9  ; Validate 1st int >= 0
-BRn ERROR
+BRn INPUT_ERROR
 
 GETC ; Input 2nd character
 
@@ -32,49 +34,49 @@ GETC ; Input 2nd character
 AND R1, R1, #0
 ADD R1, R1, #-10
 ADD R1, R0, R1
-BRz SINGLE_DIGIT
+BRz INPUT_SINGLE_DIGIT
 
 ; Integer still double-digit at this point
 
 OUT ; Output 2nd character
-LD R1, NEG_48 
+LD R1, INPUT_NEG_48 
 ADD R0, R0, R1 ; R0 = 2nd int
 
 ADD R1, R0, #-9 ; Validate 2nd int <= 9
-BRp ERROR
+BRp INPUT_ERROR
 ADD R1, R1, #9  ; Validate 2nd int >= 0
-BRn ERROR
+BRn INPUT_ERROR
 
 ; (1st int x 10) + 2nd int
 AND R1, R1, #0
 ADD R1, R1, #10
-EXPAND
+INPUT_EXPAND
 ADD R0, R0, R2
 ADD R1, R1, #-1
-BRnp EXPAND
+BRnp INPUT_EXPAND
 
 ; R0 now holds double-digit result
 
-BRnzp DOUBLE_DIGIT
+BRnzp INPUT_DOUBLE_DIGIT
 
-SINGLE_DIGIT ; Branch target if 2nd char == new line
+INPUT_SINGLE_DIGIT ; Branch target if 2nd char == new line
 AND R0, R0, #0
 ADD R0, R0, R2 ; Store first digit in R0
 
-DOUBLE_DIGIT ; Branch target if 2nd char == integer
+INPUT_DOUBLE_DIGIT ; Branch target if 2nd char == integer
 
 ; Callee loads
-LD R1, SAVE_R1
-LD R2, SAVE_R2
-LD R7, SAVE_R7
+LD R1, INPUT_SAVE_R1
+LD R2, INPUT_SAVE_R2
+LD R7, INPUT_SAVE_R7
 
 RET
 
 ; GETNUM VARS - - - - - - - - - - - - - - - - -
-SAVE_R1   .BLKW #1
-SAVE_R2   .BLKW #1
-SAVE_R7   .BLKW #1
-NEG_48    .FILL xFFD0
-ERROR_MSG .STRINGZ "Invalid input! Stay within (0-99): "
+INPUT_SAVE_R1   .BLKW #1
+INPUT_SAVE_R2   .BLKW #1
+INPUT_SAVE_R7   .BLKW #1
+INPUT_NEG_48    .FILL xFFD0
+INPUT_INPUT_ERROR_MSG .STRINGZ "Invalid input! Stay within (0-99): "
 
 .END
--- Lab06/output.asm
+++ Lab06/output.asm
@@ -1,20 +1,22 @@
 .ORIG x5000
 
-; Save registers
-ST R1, SAVE_R1
-ST R2, SAVE_R2
-ST R7, SAVE_R7
+TRAP_OUTPUT
 
-LEA R2, NUMS    ; R2 = address of NUMS
+; Save registers
+ST R1, OUTPUT_SAVE_R1
+ST R2, OUTPUT_SAVE_R2
+ST R7, OUTPUT_SAVE_R7
+
+LEA R2, OUTPUT_NUMS    ; R2 = address of OUTPUT_NUMS
 
 ; Initialize new quotient
-NEW_QUOTIENT
+OUTPUT_NEW_QUOTIENT
 AND R1, R1, #0  ; R1 = 0
 
 ; Subtract 10 from quotient
-SUB10
+OUTPUT_SUB10
 ADD R0, R0, #-10
-BRzp BUILD_QUOTIENT
+BRzp OUTPUT_BUILD_QUOTIENT
 
 ; Quotient found - store remainder in array
 ADD R0, R0, #10
@@ -22,45 +24,45 @@ STR	R0, R2, #0
 
 ; Branch if quotient = 0
 ADD R1, R1, #0
-BRz DISPLAY_NEXT
+BRz OUTPUT_DISPLAY_NEXT
 
 ; Prepare for branch
-ADD R2, R2, #1 ; Increment NUMS address
+ADD R2, R2, #1 ; Increment OUTPUT_NUMS address
 ADD R0, R1, #0 ; R0 = quotient
-BRnzp NEW_QUOTIENT
+BRnzp OUTPUT_NEW_QUOTIENT
 
 ; Accumulate quotient
-BUILD_QUOTIENT
+OUTPUT_BUILD_QUOTIENT
 ADD R1, R1, #1
-BRnzp SUB10
+BRnzp OUTPUT_SUB10
 
 ; Output next char
-DISPLAY_NEXT
+OUTPUT_DISPLAY_NEXT
 LDR R0, R2, #0  ; Load int from current address
-LD  R1, CHAR_ZERO  ; R1 = 48
+LD  R1, OUTPUT_CHAR_ZERO  ; R1 = 48
 ADD R0, R0, R1  ; Convert to char
 OUT             ; Output char
 
 ; Prepare for branch
 ADD R2, R2, #-1 ; Decrement char array address
-LEA R1, NUMS    ; Load NUMS address
+LEA R1, OUTPUT_NUMS    ; Load OUTPUT_NUMS address
 NOT R1, R1      
-ADD R1, R1, #1  ; R1 = -NUMS address
-ADD R0, R2, R1  ; Current address >= NUMS address ???
-BRzp DISPLAY_NEXT
+ADD R1, R1, #1  ; R1 = -OUTPUT_NUMS address
+ADD R0, R2, R1  ; Current address >= OUTPUT_NUMS address ???
+BRzp OUTPUT_DISPLAY_NEXT
 
 ; Restore registers
-LD R1, SAVE_R1
-LD R2, SAVE_R2
-LD R7, SAVE_R7
+LD R1, OUTPUT_SAVE_R1
+LD R2, OUTPUT_SAVE_R2
+LD R7, OUTPUT_SAVE_R7
 
 RET
 
 ; Variables
-SAVE_R1     .BLKW #1
-SAVE_R2     .BLKW #1
-SAVE_R7     .BLKW #1
-CHAR_ZERO   .FILL x0030
-NUMS        .BLKW #5 ; Store result as individual nums
+OUTPUT_SAVE_R1     .BLKW #1
+OUTPUT_SAVE_R2     .BLKW #1
+OUTPUT_SAVE_R7     .BLKW #1
+OUTPUT_CHAR_ZERO   .FILL x0030
+OUTPUT_NUMS        .BLKW #5 ; Store result as individual OUTPUT_NUMS
 
 .END
