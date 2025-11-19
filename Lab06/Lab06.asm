246117c06db003f11f33506a69283e8b3c9abd01
joeesmithh <josmth3030@gmail.com>
2025-11-18 22:30:24 -0500

    lab06: finish lab

--- Lab06/Lab06.asm
+++ Lab06/Lab06.asm
@@ -35,27 +35,27 @@ ADD R5, R6, #0          ; R5 = x5FFE
 
 ; Push node_t *head variable
 ADD R6, R6, #-1         ; R6 = x5FFD
+
+; node_t *head = 0
 AND R0, R0, #0          ; R0 = 0
-STR R0, R6, #0
+STR R0, R6, #0          ; node_t *head = 0
 
 ; Push char selection variable
 ADD R6, R6, #-1         ; R6 = x5FFC
-
-; Main menu loop
-; ---------------------------------------------------------------
 
 ; do
 ; ------------------------------
 DO
 
-; Menu prompt
+; printf("Available options:\n");
+; ...
 LEA R0, PROMPT_MENU
 PUTS
 
-; Input
-GETC
+; scanf(" %c", &selection)
+GETC                    ; R0 = input
 OUT
-STR	R0,	R5, #-2
+STR	R0,	R5, #-2         ; selection = input
 
 ; Print newline
 LD R0, NEWLINE
@@ -63,15 +63,15 @@ OUT
 
 ; if(selection == 'p')
 ; ------------------------------
-LD R1, NEG_LOWERCASE_P
-LDR R0, R5, #-2
-ADD R0, R0, R1
+LD R1, NEG_LOWERCASE_P  ; R1 = -'p'
+LDR R0, R5, #-2         ; R0 = selection
+ADD R0, R0, R1          ; selection - 'p'
 BRnp ELSE_IF_A          ; selection != 'p'
 
-    ; node_t **head parameter
+    ; Push node_t **head parameter
     ADD R6, R6, #-1         ; R6 = x5FFB
-    ADD R0, R5, #-1         ; R0 = node_t *head address
-    STR R0, R6, #0          ; R0 → x5FFB
+    ADD R0, R5, #-1         ; R0 = &head
+    STR R0, R6, #0          ; **head = &head
 
     ; printf("Contents of the linked list: ")
     LEA R0, PROMPT_PRINT
@@ -88,7 +88,7 @@ BRnp ELSE_IF_A          ; selection != 'p'
     LD R0, NEWLINE
     OUT
 
-    ; Pop if(selection == 'p') variables
+    ; Pop node_t **head parameter
     ADD R6, R6, #1          ; R6 = x5FFC
 
     BRnzp WHILE
@@ -96,31 +96,39 @@ BRnp ELSE_IF_A          ; selection != 'p'
 ; else if(selection == 'a')
 ; ------------------------------
 ELSE_IF_A
-LD R1, NEG_LOWERCASE_A
-LDR R0, R5, #-2
-ADD R0, R0, R1
+LD R1, NEG_LOWERCASE_A  ; R1 = -'a'
+LDR R0, R5, #-2         ; R0 = selection
+ADD R0, R0, R1          ; selection - 'a'
 BRnp ELSE_IF_R          ; selection != 'a'
 
     ; Push int a variable
     ADD R6, R6, #-1         ; R6 = x5FFB
 
-    ; Input number
+    ; int a = 0
+    AND R0, R0, #0          ; R0 = 0
+    STR R0, R6, #0
+
+    ; printf("Type a number to add: ")
     LEA R0, PROMPT_ADD      ; Print input prompt
     PUTS
-    TRAP x40                ; R0 = user number → x5FFB
-    STR R0, R6, #0
+
+    ; scanf("%d", &a)
+    TRAP x40                ; R0 = input
+    STR R0, R6, #0          ; a = input
+
+    ; Print newline
     LD R0, NEWLINE
-    OUT                     ; Print newline
+    OUT
 
     ; Push int a parameter
     ADD R6, R6, #-1         ; R6 = x5FFA
-    LDR R0, R5, #-3         ; R0 = int a → x5FFA
-    STR R0, R6, #0
+    LDR R0, R5, #-3         ; R0 = a
+    STR R0, R6, #0          ; a param = a
 
     ; Push node_t **head parameter
-    ADD R6, R6, #-1         ; R6 = x5FF9
-    ADD R0, R5, #-1         ; R0 = node_t *head address → x5FF9
-    STR R0, R6, #0
+    ADD R6, R6, #-1         ; R6 = x5FFB
+    ADD R0, R5, #-1         ; R0 = &head
+    STR R0, R6, #0          ; **head = &head
 
     ; addValue(&head, a)
     JSR ADD_VALUE
@@ -129,7 +137,7 @@ BRnp ELSE_IF_R          ; selection != 'a'
     LD R0, NEWLINE
     OUT
 
-    ; Pop if(selection == 'a') variables
+    ; Pop **head, a param, and a
     ADD R6, R6, #3          ; R6 = x5FFC
 
     ; Branch unconditionally to while check
@@ -138,31 +146,39 @@ BRnp ELSE_IF_R          ; selection != 'a'
 ; else if(selection == 'r')
 ; ------------------------------
 ELSE_IF_R
-LD R1, NEG_LOWERCASE_R
-LDR R0, R5, #-2
-ADD R0, R0, R1
+LD R1, NEG_LOWERCASE_R  ; R1 = -'r'
+LDR R0, R5, #-2         ; R0 = selection
+ADD R0, R0, R1          ; selection - 'r'
 BRnp WHILE              ; selection != 'r'
 
     ; Push int r variable
     ADD R6, R6, #-1         ; R6 = x5FFB
 
-    ; Input number
+    ; int r = 0
+    AND R0, R0, #0          ; R0 = 0
+    STR R0, R6, #0
+
+    ; printf("Type a number to remove: ")
     LEA R0, PROMPT_REMOVE   ; Print input prompt
     PUTS
-    TRAP x40                ; R0 = user number → x5FFB
-    STR R0, R6, #0
+
+    ; scanf("%d", &r)
+    TRAP x40                ; R0 = input
+    STR R0, R6, #0          ; r = input
+
+    ; Print newline
     LD R0, NEWLINE
-    OUT                     ; Print newline
+    OUT
 
     ; Push int r parameter
     ADD R6, R6, #-1
-    LDR R0, R5, #-3         ; R0 = int r → stack
-    STR R0, R6, #0
+    LDR R0, R5, #-3         ; R0 = r
+    STR R0, R6, #0          ; r param = r
 
     ; Push node_t **head parameter
-    ADD R6, R6, #-1
-    ADD R0, R5, #-1         ; R0 = node_t **head → stack
-    STR R0, R6, #0
+    ADD R6, R6, #-1         ; R6 = x5FFB
+    ADD R0, R5, #-1         ; R0 = &head
+    STR R0, R6, #0          ; **head = &head
 
     ; removeValue(&head, r);
     JSR REMOVE_VALUE
@@ -171,19 +187,16 @@ BRnp WHILE              ; selection != 'r'
     LD R0, NEWLINE
     OUT
 
-    ; Pop if(selection == 'r') variables
+    ; Pop ** head, r param, r
     ADD R6, R6, #3
 
 ; while(selection != 'q');
 ; ------------------------------
 WHILE
-LD R1, NEG_LOWERCASE_Q
-LDR R0, R5, #-2
-ADD R0, R0, R1
+LD R1, NEG_LOWERCASE_Q  ; R1 = -'q'
+LDR R0, R5, #-2         ; R0 = selection
+ADD R0, R0, R1          ; selection - 'q'
 BRnp DO                 ; selection != 'q'
-
-; End main menu loop
-; ---------------------------------------------------------------
 
 ; Print newline
 LD R0, NEWLINE
@@ -234,11 +247,13 @@ ADD R6, R6, #-1
 STR R5, R6, #0  
 ADD R5, R6, #0 
 
-; Push node_t *current
+; Push node_t *current variable
 ADD R6, R6, #-1
+
+; node_t *current = *head
 LDR R0, R5, #2          ; R0 = **head
 LDR R0, R0, #0          ; R0 = *head
-STR R0, R6, #0
+STR R0, R6, #0          ; *current = *head
 
 ; while(current != 0)
 ; ------------------------------
@@ -254,8 +269,7 @@ BRz PRINT_LIST_RETURN
 
     ; current = current->next
     LDR R0, R5, #-1         ; R0 = *current
-    ADD R0, R0, #1          ; R0 = *current + 1
-    LDR R0, R0, #0          ; R0 = current->next
+    LDR R0, R0, #1          ; R0 = current->*next
     STR R0, R5, #-1         ; *current = *current->next
 
     ; if(current != 0)
@@ -268,7 +282,8 @@ BRz PRINT_LIST_RETURN
         LEA R0, PRINT_LIST_ARROW
         PUTS
 
-    ; Branch unconditionally to while(current != 0)
+    ; 
+    ; Continue to while(current != 0)
     BRnzp PRINT_LIST_WHILE
 
 ; return
@@ -318,44 +333,45 @@ ADD R5, R6, #0
 ADD R6, R6, #-1
 STR R1, R6, #0
 
-; Push R2
+; Push node_t *current variable
 ADD R6, R6, #-1
-STR R2, R6, #0
 
-; Push node_t *current
-ADD R6, R6, #-1
+; node_t *current = *head
 LDR R0, R5, #2          ; R0 = **head
 LDR R0, R0, #0          ; R0 = *head
 STR R0, R6, #0
 
 ; if(current == 0)
 ; ------------------------------
+LDR R0, R5, #-2         ; R0 = *current
 ADD R0, R0, #0
 BRnp ADD_VALUE_WHILE
 
-    ; Push node_t *newNode malloc
+    ; Push node_t *newNode variable
     ADD R6, R6, #-1
-    LDR R0, R4, #1 ; R0 = current global malloc address
-    STR R0, R6, #0 ; *newNode = R0
-    ADD R0, R0, #2 ; R0 = R0 + 2
-    STR R0, R4, #1 ; global malloc address = R0
+
+    ; node_t *newNode = (node_t *) malloc(sizeof(node_t))
+    LDR R0, R4, #1 ; R0 = global malloc address
+    STR R0, R6, #0 ; *newNode = global malloc address
+    ADD R0, R0, #2 ; global malloc address  += 2
+    STR R0, R4, #1 ; global malloc variable = global malloc address  += 2
 
     ; newNode->value = added
-    LDR R0, R5, #-4         ; R0 = *newNode
+    LDR R0, R5, #-3         ; R0 = *newNode
     LDR R1, R5, #3          ; R1 = added
-    STR R1, R0, #0          ; *newNode->value = R1
+    STR R1, R0, #0          ; newNode->value = added
 
     ; newNode->next = 0
-    ADD R0, R0, #1          ; R0 = *newNode + 1
+    LDR R0, R5, #-3         ; R0 = *newNode
     AND R1, R1, #0          ; R1 = 0
-    STR R1, R0, #0          ; *newNode->next = 0
+    STR R1, R0, #1          ; newNode->*next = 0
 
     ; *head = newNode
     LDR R0, R5, #2          ; R0 = **head
-    LDR R1, R5, #-4          ; R1 = *newNode
+    LDR R1, R5, #-3          ; R1 = *newNode
     STR R1, R0, #0          ; *head = *newNode
 
-    ; Pop node_t *newNode
+    ; Pop node_t *newNode variable
     ADD R6, R6, #1
 
     ; Branch unconditionally to function return
@@ -364,53 +380,54 @@ BRnp ADD_VALUE_WHILE
 ; while(current != 0)
 ; ------------------------------
 ADD_VALUE_WHILE
-LDR R0, R5, #-3         ; R0 = *current
+LDR R0, R5, #-2         ; R0 = *current
 ADD R0, R0, #0
 BRz ADD_VALUE_RETURN
 
     ; if(current->next == 0)
     ; ------------------------------
-    ADD R0, R0, #1          ; R0 = *current + 1
-    LDR R0, R0, #0          ; R0 = current->next
+    LDR R0, R5, #-2         ; R0 = *current
+    LDR R0, R0, #1          ; R0 = current->*next
     ADD R0, R0, #0
     BRnp ADD_VALUE_WHILE_CONTINUE
 
-        ; Push node_t *newNode
+        ; Push node_t *newNode variable
         ADD R6, R6, #-1
-        LDR R0, R4, #1 ; R0 = current global malloc address
-        STR R0, R6, #0 ; *newNode = R0
-        ADD R0, R0, #2 ; R0 = R0 + 2
-        STR R0, R4, #1 ; global malloc address = R0
+
+        ; node_t *newNode = (node_t *) malloc(sizeof(node_t))
+        LDR R0, R4, #1 ; R0 = global malloc address
+        STR R0, R6, #0 ; *newNode = global malloc address
+        ADD R0, R0, #2 ; global malloc address  += 2
+        STR R0, R4, #1 ; global malloc variable = global malloc address  += 2
 
             ; newNode->value = added
-            LDR R0, R5, #-4         ; R0 = *newNode
+            LDR R0, R5, #-3         ; R0 = *newNode
             LDR R1, R5, #3          ; R1 = added
-            STR R1, R0, #0          ; *newNode->value = R1
+            STR R1, R0, #0          ; *newNode->value = added
 
             ; newNode->next = 0
-            ADD R0, R0, #1          ; R0 = *newNode + 1
+            LDR R0, R5, #-3         ; R0 = *newNode
             AND R1, R1, #0          ; R1 = 0
-            STR R1, R0, #0          ; *newNode->next = 0
+            STR R1, R0, #1          ; newNode->*next = 0
 
             ; current->next = newNode
-            LDR R0, R5, #-3         ; R0 = *currentNode
-            ADD R0, R0, #1          ; R0 = *currentNode + 1
-            LDR R1, R5, #-4         ; R1 = *newNode
-            STR R1, R0, #0          ; *currentNode->next = *newNode
+            LDR R0, R5, #-2         ; R0 = *currentNode
+            LDR R1, R5, #-3         ; R1 = *newNode
+            STR R1, R0, #1          ; currentNode->*next = *newNode
 
-        ; Pop node_t *newNode
+        ; Pop node_t *newNode variable
         ADD R6, R6, #1
 
-    ; Branch unconditionally to function return
-    BRnzp ADD_VALUE_RETURN
+        ; return
+        BRnzp ADD_VALUE_RETURN
 
+    ; current = current->next
     ADD_VALUE_WHILE_CONTINUE
-    LDR R0, R5, #-3         ; R0 = *current
-    ADD R0, R0, #1          ; R0 = *current + 1
-    LDR R0, R0, #0          ; R0 = current->next
-    STR R0, R5, #-3         ; *current = *current->next
+    LDR R0, R5, #-2         ; R0 = *current
+    LDR R0, R0, #1          ; R0 = current->*next
+    STR R0, R5, #-2         ; *current = *current->next
 
-    ; Branch unconditionally to while(current != 0)
+    ; Continue while(current != 0)
     BRnzp ADD_VALUE_WHILE
 
 ; return
@@ -418,10 +435,6 @@ BRz ADD_VALUE_RETURN
 ADD_VALUE_RETURN
 
 ; Pop node_t *current
-ADD R6, R6, #1
-
-; Pop R2
-LDR R2, R6, #0
 ADD R6, R6, #1
 
 ; Pop R1
@@ -463,17 +476,112 @@ ADD R5, R6, #0
 ADD R6, R6, #-1
 STR R1, R6, #0
 
-; Push R2
+; Push node_t *prev (node_t *prev = *head)
 ADD R6, R6, #-1
-STR R2, R6, #0
+LDR R0, R5, #2          ; R0 = **head
+LDR R0, R0, #0          ; R0 = *head
+STR R0, R6, #0          ; *prev = *head
+
+; Push node_t *current
+ADD R6, R6, #-1
+
+; if(prev == 0)
+; ------------------------------
+LDR R0, R5, #-2         ; R0 = *prev
+ADD R0, R0, #0
+BRz REMOVE_VALUE_RETURN;return
 
 
+; if((*head)->value == removed)
+; ------------------------------
+LDR R0, R5, #2          ; R0 = **head
+LDR R0, R0, #0          ; R0 = *head
+LDR R0, R0, #0          ; R0 = head->value
+LDR R1, R5, #3          ; R1 = removed
+NOT R1, R1
+ADD R1, R1, #1          ; R1 = -removed
+ADD R0, R0, R1
+BRnp REMOVE_VALUE_CONTINUE
+
+    ; (*head) = (*head)->next
+    LDR R0, R5, #2          ; R0 = **head
+    ADD R1, R0, #0          ; R1 = **head
+    LDR R1, R1, #0          ; R1 = *head
+    LDR R1, R1, #1          ; R1 = head->*next
+    STR R1, R0, #0          ; (*head) = (*head)->*next
+
+    ; free(prev); 
+    LDR R0, R5, #-2         ; R0 = *prev
+    AND R1, R1, #0          ; R1 = 0
+    STR R1, R0, #0          ; prev->value = 0
+    STR R1, R0, #1          ; prev->*next = 0
+
+    ; return
+    BRnzp REMOVE_VALUE_RETURN
+
+; node_t *current = (*head)->next
+REMOVE_VALUE_CONTINUE
+LDR R0, R5, #2              ; R0 = **head
+LDR R0, R0, #0              ; R0 =  *head
+LDR R0, R0, #1              ; R0 = head->*next
+STR R0, R5, #-3             ; current = (*head)->*next
 
 
+; while(current != 0)
+; ------------------------------
+REMOVE_VALUE_WHILE
+LDR R0, R5, #-3             ; R0 = current
+ADD R0, R0, #0
+BRz REMOVE_VALUE_RETURN
 
-; Pop R2
-LDR R2, R6, #0
-ADD R6, R6, #1
+    ; if(current->value == removed)
+    ; ------------------------------
+    LDR R0, R5, #-3             ; R0 = *current
+    LDR R0, R0, #0              ; R0 = current->value
+    LDR R1, R5, #3              ; R1 = removed
+    NOT R1, R1
+    ADD R1, R1, #1              ; R1 = -removed
+    ADD R0, R0, R1              ; current->value - removed
+    BRnp REMOVE_VALUE_WHILE_IF2
+
+        ; prev->next = current->next
+        LDR R0, R5, #-3             ; R0 = *current
+        LDR R0, R0, #1              ; R0 = current->*next
+        LDR R1, R5, #-2             ; R1 = *prev
+        STR R0, R1, #1              ; prev->*next = current->*next
+
+        ; free(current)
+        LDR R0, R5, #-3             ; R0 = *current
+        AND R1, R1, #0              ; R1 = 0
+        STR R1, R0, #0              ; current->value = 0
+        STR R1, R0, #1              ; current->*next  = 0
+
+        ; return
+        BRnzp REMOVE_VALUE_RETURN
+
+    ; if (current != 0)
+    ; ------------------------------
+    REMOVE_VALUE_WHILE_IF2
+    LDR R0, R5, #-3             ; R0 = *current
+    ADD R0, R0, #0
+    BRz REMOVE_VALUE_WHILE      
+
+        ; prev = current
+        LDR R0, R5, #-3         ; R0 = *current
+        STR R0, R5, #-2         ; *prev = *current
+
+        ; current = current->next
+        LDR R0, R5, #-3         ; R0 = *current
+        LDR R0, R0, #1          ; R0 = current->next
+        STR R0, R5, #-3         ; *current = current->*next
+
+    BRnzp REMOVE_VALUE_WHILE
+
+; return
+REMOVE_VALUE_RETURN
+
+; Pop local vars
+ADD R6, R6, #2
 
 ; Pop R1
 LDR R1, R6, #0
