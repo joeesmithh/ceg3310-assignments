9380cf4e5abd139c62b57211f52e30b29796e3e2
joeesmithh <josmth3030@gmail.com>
2025-12-05 16:25:41 -0500

    create final project files

--- Lab02/array.asm
+++ Lab02/array.asm
@@ -2,8 +2,11 @@
 
 ; Loading
 LD R1, ARRAY_SIZE
-LEA R2, ARRAY_START
-LD R3, NEG_48
+NOT R1
+ADD R1, R1, #1              ; R1, Negated array size
+LEA R2, ARRAY_START         ; R2, Array start address
+LD R3, NEG_48               ; R3, #-48
+AND R4, R4, #0              ; R4, Loop iterator
 
 LOOP_START LEA R0, PROMPT   ; Load prompt string address into r0
 PUTS                        ; Print string starting at R0
@@ -13,7 +16,7 @@ STR R0, R2, #0              ; Store R0 value at address stored in R2
 LD R0, NEWLINE_CHAR         ; Store NEWLINE-CHAR in R0
 TRAP x21                    ; OUT - Print new line to console
 ADD R2, R2, #1              ; Add 1 to address stored in R2
-ADD R1, R1, #-1             ; Decrement R1 (ARRAY_SIZE)
+ADD R4, R, #-1             ; Decrement R1 (ARRAY_SIZE)
 BRnp LOOP_START
 
 HALT
